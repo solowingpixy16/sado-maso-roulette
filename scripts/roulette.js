51f3b715ade28de4bc8dddb94b8819ee276ef2c6
pixy <205641822+solowingpixy16@users.noreply.github.com>
2025-04-01 05:16:18 +0900

    Succeeded to rotate the indicator of roulette.

--- scripts/roulette.js
+++ scripts/roulette.js
@@ -15,43 +15,62 @@ const CONTEXT = ROULETTE_CANVAS.getContext("2d");
 
 const CENTER_X = ROULETTE_CANVAS.clientWidth / 2.0;
 const CENTER_Y = ROULETTE_CANVAS.clientHeight / 2.0;
-let radius;
+let roulette_radius;
 if (ROULETTE_CANVAS.clientWidth <= ROULETTE_CANVAS.clientHeight) {
-    radius = ROULETTE_CANVAS.clientWidth / 2.0;
+    roulette_radius = ROULETTE_CANVAS.clientWidth / 2.0 - 20;
 } else {
-    radius = ROULETTE_CANVAS.clientHeight / 2.0;
+    roulette_radius = ROULETTE_CANVAS.clientHeight / 2.0 - 20;
 }
+
+const INDICATOR_CIRCLE_RADIUS = 10;
+const INDICATOR_ORBIT_RADIUS = roulette_radius + 10;
+const INDICATOR_SPEED = 0.02;
 
 const PIECES_COUNT = 16;
 
-// サド用の扇形を描画
-for (let i = 0; i < PIECES_COUNT; i+=2) {
+let current_indicator_angle = - 1.0/2.0 * Math.PI;
+function draw() {
+    CONTEXT.clearRect(0, 0, ROULETTE_CANVAS.clientWidth, ROULETTE_CANVAS.clientHeight);
+    // サド用の扇形を描画
+    for (let i = 0; i < PIECES_COUNT; i+=2) {
+        CONTEXT.beginPath();
+        // まずは点を中心に置く
+        CONTEXT.moveTo(CENTER_X, CENTER_Y);
+        // 一旦中心から円周に向かって半径を描画して、その後円弧を描く
+        CONTEXT.arc(CENTER_X, CENTER_Y, roulette_radius, i / 8.0 * Math.PI, (i + 1.0) / 8.0 * Math.PI, false);
+        // 円周から中心に向かって半径を描画する。これでパスが閉じて元の位置に戻る。
+        CONTEXT.lineTo(CENTER_X, CENTER_Y);
+        CONTEXT.fillStyle = "blue";
+        CONTEXT.fill();
+        CONTEXT.closePath();
+    }
+
+    // マゾ用の扇形を描画
+    for (let i = 1; i < PIECES_COUNT; i+=2) {
+        // なーんかコピペコードになってて嫌
+        // 1個の扇形を描画する処理を関数化、あと扇を一定間隔で埋め尽くす処理を関数化して処理を共通化したい
+        // でも4月1日に間に合うことが正義なのでもうどうでもいい
+        CONTEXT.beginPath();
+        // まずは点を中心に置く
+        CONTEXT.moveTo(CENTER_X, CENTER_Y);
+        // 一旦中心から円周に向かって半径を描画して、その後円弧を描く
+        CONTEXT.arc(CENTER_X, CENTER_Y, roulette_radius, i / 8.0 * Math.PI, (i + 1.0) / 8.0 * Math.PI, false);
+        // 円周から中心に向かって半径を描画する。これでパスが閉じて元の位置に戻る。
+        CONTEXT.lineTo(CENTER_X, CENTER_Y);
+        CONTEXT.fillStyle = "red";
+        CONTEXT.fill();
+        CONTEXT.closePath();
+    }
+
+    current_indicator_angle += INDICATOR_SPEED;
+    let current_indicator_x = CENTER_X + INDICATOR_ORBIT_RADIUS * Math.cos(current_indicator_angle);
+    let current_indicator_y = CENTER_Y + INDICATOR_ORBIT_RADIUS * Math.sin(current_indicator_angle);
     CONTEXT.beginPath();
-    // まずは点を中心に置く
-    CONTEXT.moveTo(CENTER_X, CENTER_Y);
-    // 一旦中心から円周に向かって半径を描画して、その後円弧を描く
-    CONTEXT.arc(CENTER_X, CENTER_Y, radius, i / 8.0 * Math.PI, (i + 1.0) / 8.0 * Math.PI, false);
-    // 円周から中心に向かって半径を描画する。これでパスが閉じて元の位置に戻る。
-    CONTEXT.lineTo(CENTER_X, CENTER_Y);
-    CONTEXT.fillStyle="blue";
+    CONTEXT.moveTo(current_indicator_x, current_indicator_y);
+    CONTEXT.arc(current_indicator_x, current_indicator_y, INDICATOR_CIRCLE_RADIUS, 0, 2.0 * Math.PI, false);
+    CONTEXT.fillStyle = "yellow";
     CONTEXT.fill();
     CONTEXT.closePath();
 }
 
-// マゾ用の扇形を描画
-for (let i = 1; i < PIECES_COUNT; i+=2) {
-    // なーんかコピペコードになってて嫌
-    // 1個の扇形を描画する処理を関数化、あと扇を一定間隔で埋め尽くす処理を関数化して処理を共通化したい
-    // でも4月1日に間に合うことが正義なのでもうどうでもいい
-    CONTEXT.beginPath();
-    // まずは点を中心に置く
-    CONTEXT.moveTo(CENTER_X, CENTER_Y);
-    // 一旦中心から円周に向かって半径を描画して、その後円弧を描く
-    CONTEXT.arc(CENTER_X, CENTER_Y, radius, i / 8.0 * Math.PI, (i + 1.0) / 8.0 * Math.PI, false);
-    // 円周から中心に向かって半径を描画する。これでパスが閉じて元の位置に戻る。
-    CONTEXT.lineTo(CENTER_X, CENTER_Y);
-    CONTEXT.fillStyle="red";
-    CONTEXT.fill();
-    CONTEXT.closePath();
-}
-
+setInterval(draw, 10);
